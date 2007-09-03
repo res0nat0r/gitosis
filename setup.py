e492d76c29f41c557042a09b4886ab4df67d4c0d
Tommi Virtanen <tv@eagain.net>
2007-09-03 14:06:51 -0700

    Make setuptools include templates in the egg.

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python
 from setuptools import setup, find_packages
+import os
+
+def _subdir_contents(path):
+    for toplevel in os.listdir(path):
+        toplevel_path = os.path.join(path, toplevel)
+        if not os.path.isdir(toplevel_path):
+            continue
+        for dirpath, dirnames, filenames in os.walk(toplevel_path):
+            for filename in filenames:
+                full_path = os.path.join(dirpath, filename)
+                if not full_path.startswith(path+'/'):
+                    raise RuntimeError()
+                yield full_path[len(path)+1:]
+def subdir_contents(path):
+    return list(_subdir_contents(path))
+
 setup(
     name = "gitosis",
     version = "0.1",
@@ -21,4 +37,11 @@ setup(
             'gitosis-init = gitosis.init:main',
             ],
         },
+
+    package_data = {
+        # this seems to be the only way to convince setuptools
+        # to include things recursively
+        'gitosis.templates': subdir_contents('gitosis/templates'),
+    },
     )
+
